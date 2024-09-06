455b8d38a78135da06e9c4289d0823a753c3cda1
Theredhawksings <168459055+Theredhawksings@users.noreply.github.com>
2024-09-06 12:27:40 +0900

    워밍업 3 끝
    ㅋㅋㅋ

--- 비상용/비상용/c언어.cpp
+++ 비상용/비상용/c언어.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
-#include <deque>  // std::deque 사용
+#include <deque> 
+#include <algorithm> 
+
+
 using namespace std;
 
 struct Point {
@@ -8,8 +11,10 @@ struct Point {
     int z;
 };
 
+bool as = true;
+
 void add_point(deque<Point>& points, int x, int y, int z) {
-    points.push_front({ x, y, z });  // 맨 위에 추가
+    points.push_front({ x, y, z });  
 }
 
 void remove_point_front(deque<Point>& points) {
@@ -40,7 +45,7 @@ void remove_point_end(deque<Point>& points) {
     }
 }
 
-void print_count(deque<Point>& points) {
+void print_size(deque<Point>& points) {
     cout << points.size() << endl;
 }
 
@@ -50,14 +55,69 @@ void remove_all_point(deque<Point>& points) {
     }
 }
 
+void print_front(deque<Point>& points) {
+    if (!points.empty()) {
+        Point& frontPoint = points.front();
+        cout << frontPoint.x << ", " << frontPoint.y << ", " << frontPoint.z << endl;
+    }
+    else {
+        cout << "비어 있습니다" << endl;
+    }
+}
+
+void print_back(deque<Point>& points) {
+    if (!points.empty()) {
+        Point& backPoint = points.back();
+        cout << backPoint.x << ", " << backPoint.y << ", " << backPoint.z << endl;
+    }
+    else {
+        cout << "비어 있습니다" << endl;
+    }
+}
+
+bool compareAscending(const Point& a, const Point& b) {
+    if (a.x != b.x) return a.x < b.x;
+    if (a.y != b.y) return a.y < b.y;
+    return a.z < b.z;
+}
+
+bool compareDescending(const Point& a, const Point& b) {
+    if (a.x != b.x) return a.x > b.x;
+    if (a.y != b.y) return a.y > b.y;
+    return a.z > b.z;
+}
+
+void print_sorted_ascending(const deque<Point>& points) {
+    deque<Point> pointss = points; 
+    sort(pointss.begin(), pointss.end(), compareAscending);
+    cout << "오름차순 정렬:\n";
+    print_points(pointss);
+}
+
+void print_sorted_descending(const deque<Point>& points) {
+    deque<Point> pointss = points;  
+    sort(pointss.begin(), pointss.end(), compareDescending);
+    cout << "내림차순 정렬:\n";
+    print_points(pointss);
+}
+
 int main() {
     deque<Point> points;
 
     string command;
 
+    points.push_front({ 1, 1, 0 });
+    points.push_front({ 1, 0, 0 });
+    points.push_front({ 1, 1, 1 });
+    print_points(points);
+
+
     while (1) {
-        cout << "명령어 입력     ";
+        
+        cout << "명령어 입력     " << endl << endl;
         cin >> command;
+
+        
 
         if (command == "+") {
             int x, y, z;
@@ -84,13 +144,46 @@ int main() {
         }
 
         else if (command == "l") {
-            print_count(points);
+            print_size(points);
         }
 
         else if (command == "c") {
             remove_all_point(points);
         }
+
+        else if (command == "m") {
+            print_front(points);
+        }
+
+        else if (command == "n") {
+            print_back(points);
+        }
+        else if (command == "a") {
+
+            if (as == true) {
+                print_sorted_ascending(points);
+            }
+            else  if (as == false) {
+                print_points(points);
+            }
+            
+            as = !as;
+
+        }
+        else if (command == "s") {
+
+            if (as == true) {
+                print_sorted_descending(points);
+            }
+
+            else  if (as == false) {
+                print_points(points);
+            }
+
+            as = !as;
+        }
     }
+
 
     return 0;
 }
